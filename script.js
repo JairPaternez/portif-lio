dcf9c9c3a87e7e48aa7e35624f2b5cd84373def8
Jair Dias de Oliveira Paternez <117412507+JairPaternez@users.noreply.github.com>
2026-01-07 11:02:31 -0300

    Add files via upload
    subir site

---
+++ script.js
@@ -0,0 +1,15 @@
+const menuToggle = document.getElementById('menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+
+
+document.querySelectorAll('.nav-links a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        target.scrollIntoView({ behavior: 'smooth' });
+        navLinks.classList.remove('active');
+    });
+});
